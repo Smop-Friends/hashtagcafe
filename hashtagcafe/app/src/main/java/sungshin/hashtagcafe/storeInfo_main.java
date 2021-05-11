f722a66845b22d3147e30805134aa2d9c459168e
amado177 <69021925+amado177@users.noreply.github.com>
2021-05-12 06:40:53 +0900

    modify layout

--- hashtagcafe/app/src/main/java/sungshin/hashtagcafe/storeInfo_main.java
+++ hashtagcafe/app/src/main/java/sungshin/hashtagcafe/storeInfo_main.java
@@ -6,17 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class storeInfo_main extends AppCompatActivity {
 
 
-    Button mapBtn,modifyBtn;
+    ImageButton mapBtn,modifyBtn,keep;
+    Boolean keepClick=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_info_main);
 
-        mapBtn= (Button)findViewById(R.id.map);
+        mapBtn= (ImageButton) findViewById(R.id.map);
+        modifyBtn= (ImageButton)findViewById(R.id.modify);
+        keep= (ImageButton)findViewById(R.id.keep);
+
         mapBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -25,7 +30,7 @@ public class storeInfo_main extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        modifyBtn= (Button)findViewById(R.id.modify);
+
         modifyBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -35,5 +40,22 @@ public class storeInfo_main extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        keep.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //setIcon emptyHeart
+                if(keepClick)
+                {
+                    keepClick=!keepClick;
+                    keep.setImageResource(R.drawable.emptyheart);
+                }
+                else
+                {
+                    keepClick=!keepClick;
+                    keep.setImageResource(R.drawable.fullheart);
+                }
+            }});
     }
 }
--- hashtagcafe/app/src/main/java/sungshin/hashtagcafe/storeInfo_modify.java
+++ hashtagcafe/app/src/main/java/sungshin/hashtagcafe/storeInfo_modify.java
@@ -12,6 +12,20 @@ public class storeInfo_modify extends AppCompatActivity {
 
     TextView checkCovid,checkInterior,checkCafe,checkMenu;
     Boolean textCovidClick=true,textInteriorClick=true,textCafeClick=true,textMenuClick=true;
+    CheckBox [] checkBoxCovids=new CheckBox[10];
+    CheckBox [] checkBoxInteriors=new CheckBox[10];
+    CheckBox [] checkBoxCafes=new CheckBox[10];
+    CheckBox [] checkBoxMenus=new CheckBox[10];
+
+    Integer[] checkBoxCovidId={R.id.check_covid19_1,R.id.check_covid19_2,R.id.check_covid19_3,
+            R.id.check_covid19_4,R.id.check_covid19_5};
+    Integer[] checkBoxInteriorId={R.id.check_interior_1,R.id.check_interior_2,R.id.check_interior_3,R.id.check_interior_4,
+            R.id.check_interior_5,R.id.check_interior_6,R.id.check_interior_7,R.id.check_interior_8,R.id.check_interior_9};
+    Integer[] checkBoxCafeId={R.id.check_cafe_1,R.id.check_cafe_2,R.id.check_cafe_3,R.id.check_cafe_4,R.id.check_cafe_5,
+            R.id.check_cafe_6,R.id.check_cafe_7,R.id.check_cafe_8};
+    Integer[] checkBoxMenuId={R.id.check_menu_1,R.id.check_menu_2,R.id.check_menu_3,R.id.check_menu_4,R.id.check_menu_5,
+            R.id.check_menu_6,R.id.check_menu_7,R.id.check_menu_8};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +37,7 @@ public class storeInfo_modify extends AppCompatActivity {
          checkCafe=(TextView)findViewById(R.id.checkCafe);
          checkMenu=(TextView)findViewById(R.id.checkMenu);
 
-
-        CheckBox [] checkBoxCovids=new CheckBox[10];
-        CheckBox [] checkBoxInteriors=new CheckBox[10];
-        CheckBox [] checkBoxCafes=new CheckBox[10];
-        CheckBox [] checkBoxMenus=new CheckBox[10];
-
-        Integer[] checkBoxCovidId={R.id.check_covid19_1,R.id.check_covid19_2,R.id.check_covid19_3,
-                R.id.check_covid19_4,R.id.check_covid19_5};
-        Integer[] checkBoxInteriorId={R.id.check_interior_1,R.id.check_interior_2,R.id.check_interior_3,R.id.check_interior_4,
-                R.id.check_interior_5,R.id.check_interior_6,R.id.check_interior_7,R.id.check_interior_8,R.id.check_interior_9};
-        Integer[] checkBoxCafeId={R.id.check_cafe_1,R.id.check_cafe_2,R.id.check_cafe_3,R.id.check_cafe_4,R.id.check_cafe_5,
-                R.id.check_cafe_6,R.id.check_cafe_7,R.id.check_cafe_8};
-        Integer[] checkBoxMenuId={R.id.check_menu_1,R.id.check_menu_2,R.id.check_menu_3,R.id.check_menu_4,R.id.check_menu_5,
-                R.id.check_menu_6,R.id.check_menu_7,R.id.check_menu_8};
-
-
-// 체크박스 연결
+        // 체크박스 연결
         for (int i = 0; i < checkBoxCovidId.length; i++) {
             checkBoxCovids[i] = (CheckBox) findViewById(checkBoxCovidId[i]);
         }
@@ -52,6 +50,7 @@ public class storeInfo_modify extends AppCompatActivity {
         for (int i = 0; i < checkBoxMenuId.length; i++) {
             checkBoxMenus[i] = (CheckBox) findViewById(checkBoxMenuId[i]);
         }
+            //체크박스 펼치기
         checkCovid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
